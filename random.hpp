06522f875b470208113d889e42a0515345276890
rkousnetsov <rkousnetsov@users.noreply.github.com>
2016-03-06 13:21:17 -0800

    Update random.hpp

--- random.hpp
+++ random.hpp
@@ -1,12 +1,10 @@
 #ifndef RANDOM_HPP
 #define RANDOM_HPP
 
-#include <random>
-
 /*
  * This class is reserved for all random variable generation.
  * All methods are static (enables the methods to be used without class instantiation).
- * This class requires C++11.
+ * This class requires C++11 (for the <random> library).
  */
 class Random
 {
@@ -16,10 +14,6 @@ public:
     static float exponential(float lambda);
     static float weibull(float lambda, float k);
     static float gamma(float alpha, float beta);
-
-private:
-    static std::random_device rd; // Generator for random real value (as seed for RNG)
-    static std::mt19937 gen; // Random number generator, uses Mersenne Twister algorithm
 };
 
 #endif // RANDOM_HPP
